b6f096bb09fc6611c5c372e9170e1732977a71f7
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-10-08 11:20:20 +0900

    [solved] ransom note

---
+++ solutions/ransom-note.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+
+var canConstruct = function (ransomNote, magazine) {
+	const wordMap = {};
+
+	for (let i = 0; i < magazine.length; i++) {
+		const target = magazine[i];
+
+		if (wordMap[target]) {
+			wordMap[target] += 1;
+		} else {
+			wordMap[target] = 1;
+		}
+	}
+
+	for (let i = 0; i < ransomNote.length; i++) {
+		const target = ransomNote[i];
+
+		if (wordMap[target]) {
+			wordMap[target] -= 1;
+
+			if (wordMap[target] === 0) delete wordMap[target];
+		} else {
+			return false;
+		}
+	}
+
+	return true;
+};
