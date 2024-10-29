2ef3663d5bd495cac159d06bfbb555c48b184f76
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-10-29 12:36:27 +0900

    [solved] binary tree level order traversal

---
+++ solutions/binary-tree-level-order-traversal.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+var levelOrder = function(root) {
+  const depthMap = {};
+  let search = [[root, 0]];
+
+  if (root === null) return [];
+  
+  while (search.length !== 0) {
+      const [targetNode, targetDepth] = search.shift();
+
+      if (depthMap[targetDepth]) {
+          depthMap[targetDepth].push(targetNode.val);
+      } else {
+          depthMap[targetDepth] = [targetNode.val];
+      }
+
+      if (targetNode.left) search.push([targetNode.left, targetDepth + 1]);
+      if (targetNode.right) search.push([targetNode.right, targetDepth + 1]);
+  }
+
+  return Object.values(depthMap);
+};
